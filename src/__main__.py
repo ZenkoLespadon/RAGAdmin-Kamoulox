af4986cf2d34965d553c431af53df3e114c11017
Matias DEVEZE <matiasdeveze@gmail.com>
2024-11-12 10:35:41 +0100

    chore: Splitting the 'app-test-lint' pipeline

--- src/__main__.py
+++ src/__main__.py
@@ -1,1 +1,8 @@
-print("hello")
+def main()->None:
+    """
+    This module performs [brief description of the module's purpose].
+    """
+    print("hello")
+
+if __name__ == "__main__":
+    main()
