8dfa11bb377ae29f03cb31f86c0ec3fa41a44a04
Matias DEVEZE <matiasdeveze@gmail.com>
2024-11-12 10:48:13 +0100

    test: pylint test

--- src/__main__.py
+++ src/__main__.py
@@ -1,8 +1,10 @@
+"""
+    MAIN FUNCTION
+"""
+
 def main()->None:
-    """
-    This module performs [brief description of the module's purpose].
-    """
     print("hello")
 
 if __name__ == "__main__":
     main()
+    
