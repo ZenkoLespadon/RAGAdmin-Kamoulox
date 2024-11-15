5e76bb75363a41fb77bb0c39cf89592eafd8aca1
Rémi Lhuissier <remi.lhuissier@etu.unilim.fr>
2024-11-15 13:16:11 +0100

    feat: WIP copy files to mirror folder

--- src/fileManager.py
+++ src/fileManager.py
@@ -29,6 +29,34 @@ def fileOrFolder(path:str) -> str :
         #print(f"{path} n'est ni un fichier ni un dossier.")
         return "unknow"
 
+
+
+def create_missing_directories(path: str) -> None:
+    """
+    Crée tous les répertoires manquants dans le chemin donné.
+
+    :param path: Le chemin d'accès pour lequel les répertoires manquants doivent être créés.
+    """
+
+    path = r"" + replace_backslashes(event.src_path)
+    if fileOrFolder(path) == "file":  # A IMPLEMENTER
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+
+    elif fileOrFolder(path) == "folder":
+        os.makedirs(path, exist_ok=True)
+        print(f"Les répertoires manquants ont été créés pour : {path}")
+    else:
+        print(f"chemin invalide")
+
+def replaceFirstFolder(path:str,fileName:str) -> str :
+    path = os.path.normpath(path)
+    dossiers = path.split(os.sep)
+    print(len(dossiers))
+
+    if dossiers[0] != ".":
+        dossiers[0] = fileName
+    return os.path.join(*dossiers)
+
 class MyHandler(FileSystemEventHandler):
     def on_created(self, event):
         path = r"" + replace_backslashes(event.src_path)
@@ -43,20 +71,13 @@ class MyHandler(FileSystemEventHandler):
         path = r"" + replace_backslashes(event.src_path)
         print(f"Objet supprimé : ",replace_backslashes(path))
 
-    #def on_modified(self, event):
-    #    path = r"" + replace_backslashes(event.src_path)
-    #    if fileOrFolder(path) == "file":  # Éviter les dossiers
-    #        print(f"Fichier modifié : ", replace_backslashes(path))
-    #    elif fileOrFolder(path) == "folder":
-    #        print(f"Dossier modifié : ", replace_backslashes(path))
-    #    else:
-    #        print(f"Objet modifié inconnu")
 
 
 if __name__ == "__main__" :
 
     # Dossier à surveiller
-    path_to_watch = "./files"  # Remplacez par le chemin du dossier que vous voulez surveiller
+    path_to_watch = "./docs"  # Remplacez par le chemin du dossier que vous voulez surveiller
+    path_to_record = "./files"
 
     # Initialiser l'observateur
     event_handler = MyHandler()
--- src/fileOrFolder.py
+++ src/fileOrFolder.py
@@ -2,19 +2,39 @@ import os
 
 
 
-def fileOrFolder(path:str):
+def fileOrFolder(path:str) -> str :
+    """
+        Fonction qui prend en paramette un chemin d'accès
+    """
 # Vérifier si le chemin est un dossier
     if os.path.isdir(path):
-        print(f"{path} est un dossier.")
+        #print(f"{path} est un dossier.")
+        return "folder"
     # Vérifier si le chemin est un fichier
     elif os.path.isfile(path):
-        print(f"{path} est un fichier.")
+        #print(f"{path} est un fichier.")
+        return "file"
     else:
-        print(f"{path} n'est ni un fichier ni un dossier.")
+        #print(f"{path} n'est ni un fichier ni un dossier.")
+        return "unknow"
+
+def delFileOfPath(path:str):
+    return os.path.dirname(path)
+
+def replaceFirstFolder(path:str,fileName:str) -> str :
+    path = os.path.normpath(path)
+    dossiers = path.split(os.sep)
+    print(len(dossiers))
+
+    if dossiers[0] != ".":
+        dossiers[0] = fileName
+    return os.path.join(*dossiers)
 
 if __name__ == "__main__":
 
     tab = ["./test","files/Subject1","files/Subject1/test.txt"]
     for path in tab:
-        fileOrFolder(path)
+        res = delFileOfPath(path)
+        print("path = ",res, " new path = ",replaceFirstFolder(res,"docs"))
+
 
