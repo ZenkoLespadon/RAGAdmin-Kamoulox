c453aff0067ce22a8ff4ba03f139fa86424b64e5
guillaume <guillaume.frugier@etu.unilim.fr>
2024-11-18 09:07:13 +0100

    feat: adding the chromadb file that will handle the database in the future, and the indexing files that search and index every files in the doc

---
+++ src/service/chromadb/indexing.py
@@ -0,0 +1,35 @@
+import os
+
+
+def indexing_file(dir_to_search):
+    list_of_all_file = dict()
+    files_directory = os.listdir(dir_to_search)
+    temp_name_file = ""
+    files =[]
+    directories = []
+
+    #indexing files and directory
+    for i in files_directory:
+        if os.path.isdir(i):
+            #if it's a directory it's added to directories
+            directories.append(i)
+        else:
+            #if it's a file it's append here
+            files.append(i)
+
+    #
+    for i in files:
+        for j in i:
+            if j != '.':
+                temp_name_file += j
+            else:
+                list_of_all_file[temp_name_file] = i
+                temp_name_file = ""
+        temp_name_file = ""
+
+    return list_of_all_file , directories
+
+
+def indexing_pdf_files(all_files):
+    files = all_files
+
