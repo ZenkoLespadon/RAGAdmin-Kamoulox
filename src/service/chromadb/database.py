df37b201fc8c0feb788baeed4a155f9d0c8b6192
guillaume <guillaume.frugier@etu.unilim.fr>
2024-11-18 11:37:24 +0100

    feat: the database file can now instantiate collection, creation of database_utils, for function that will be usefull for creating thing based on the database (like for example generating and ID)

--- src/service/chromadb/database.py
+++ src/service/chromadb/database.py
@@ -1,4 +1,16 @@
 import chromadb
 
-def create_collection_and_instanciate_collection(db):
+from database_utils import creation_id
+
+
+def create_collection_and_instanciate_collection(db, pdf_content):
     collection = db.create_collection("documentation")
+
+    for i in pdf_content:
+        for j in i:
+            collection.add(
+                document=j,
+                ids=creation_id(j)
+            )
+
+    return  collection
